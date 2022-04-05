d7aa6f1e559f36f1800a843ab0122a90b1d92c2b
Mahmudul252 <mahmudulkabir990@gmail.com>
2022-04-04 22:13:00 -0500

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css';
 
 const Header = () => {
     return (
         <nav>
-            This is Header
+            <CustomLink to='/home'>HOME</CustomLink>
+            <CustomLink to='/reviews'>REVIEWS</CustomLink>
+            <CustomLink to='/dashboard'>DASHBOARD</CustomLink>
+            <CustomLink to='/blogs'>BLOGS</CustomLink>
+            <CustomLink to='/about'>ABOUT</CustomLink>
         </nav>
     );
 };
